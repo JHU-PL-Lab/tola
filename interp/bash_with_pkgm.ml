49a97f9418dcd5416b9b1da81e57bf658020cc60
lkostick <lkostick1@gmail.com>
2023-10-12 09:40:12 -0400

    Cleaning up expander function for shell to be a bit more idiomatic

--- interp/bash_with_pkgm.ml
+++ interp/bash_with_pkgm.ml
@@ -1,8 +1,6 @@
 open Packaging
 open Std.File_infix
 
-(* parser goes here *)
-(* TODO *)
 type tokens = String of string | Pid of string
 
 let regex = Str.regexp "#import"
@@ -11,32 +9,28 @@ let parse_import s = String.sub s (String.length "#import")
   (String.length s - String.length "#import") |> String.trim |> 
     String.split_on_char ' ' |> List.map (fun x -> Pid x)
 
-(* let rec parse = function *)
-(*   | [] -> [] *)
-(*   | hd::tl -> if Str.string_match regex hd 0 then (parse_import hd)@parse tl *)
-(*               else (String hd)::parse tl *)
 
-  let parse file = 
-    let rec parse_aux = function 
-      | [] -> []
-      | hd::tl -> if Str.string_match regex hd 0 then (parse_import hd)@parse_aux tl
-              else (String hd)::parse_aux tl
-  in file |> String.split_on_char '\n' |> List.filter (fun s -> s <> String.empty) |> parse_aux
+let parse file = 
+  let rec parse_aux = function 
+    | [] -> []
+    | hd::tl -> if Str.string_match regex hd 0 then (parse_import hd)@parse_aux tl
+            else (String hd)::parse_aux tl
+in file |> String.split_on_char '\n' |> List.filter (fun s -> s <> String.empty) |> parse_aux
 
 module Make (PM : Manager.S with type P.payload = string) = struct
   let get_string_payload pid = 
     pid |> PM.P.str_to_pid |> PM.lookup |> PM.P.payload_of_pkg
-  
-  let expander e = 
-    let rec expander_aux ret e =
+ 
+
+  let rec expander ?(ret = "") e = 
     match e with 
     | [] -> ret
     | hd::tl -> let x =
       match hd with
       | String s -> s
-      | Pid pid -> (get_string_payload pid |> parse |> expander_aux "")
-      in expander_aux (ret^x^"\n") tl
-    in expander_aux "" e
+      | Pid pid -> (get_string_payload pid |> parse |> expander)
+      in expander ~ret:(ret^x^"\n") tl
+
 end    
 
 module Basic_config : Manager.CONFIG = struct
