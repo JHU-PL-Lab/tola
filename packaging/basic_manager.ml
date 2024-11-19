9c6def830786ec6b81cd5afe92ad2c435b59b02a
Weng Shiwei <arbipher@gmail.com>
2024-11-19 13:17:23 -0500

    [WIP]Move store modole out of pkgm module

--- packaging/basic_manager.ml
+++ packaging/basic_manager.ml
@@ -4,7 +4,6 @@
    Now the question is using a file system and the structure of the file system is not bundled. The structure of a package and the structure of a store should be able in configurated.
 *)
 open Package
-open Std.File_infix
 
 (* (Table : Hashtbl.S with type key = P.pid) *)
 (* type t = P.pkg Table.t and *)
@@ -16,13 +15,8 @@ module Make
     (PC : Manager.PKG_FILE_CONFIG) : Manager.S with module P = P = struct
   module P = P
   module VL = Versioning.Version_logic.Make (P) (V)
-
-  module Table = Hashtbl.Make (struct
-    type t = P.pid
-
-    let equal = Std.fn_lift2 String.equal P.pid_to_str
-    let hash = Hashtbl.hash
-  end)
+  module Pkg_store = Store.File_store_make (P) (C) (PC)
+  module Table = Pkg_store.Table
 
   let info_of_table table =
     let pp_pid = Fmt.using P.pid_to_str Fmt.string in
@@ -32,49 +26,23 @@ module Make
 
   type t = P.pkg Table.t
 
-  module Pkg_file = struct
-    let save_pkg_content pkg_path pkg =
-      if not (Sys.file_exists pkg_path) then Sys.mkdir pkg_path 0o755;
-      Std.write_file_all (pkg_path $/ PC.file_name) (P.pkg_to_str pkg)
-
-    let load_pkg_content pkg_path = Std.read_file_all (pkg_path $/ PC.file_name)
-  end
-
-  let path_of_pid pid = C.local_root $/ P.pid_to_str pid
-  let path_of_pid_s pid_s = C.local_root $/ pid_s
-  let remote_path_of_pid pid = C.remote_root $/ P.pid_to_str pid
-
   (* A table is just a cache for the directory status *)
   let local_table = ref (Table.create 64)
   let remote_table = ref (Table.create 64)
 
   (* utilities *)
-  let set_store table' = local_table := table'
-
-  let load_store pkgm_root =
-    let pid_and_pkgs =
-      Sys.readdir pkgm_root |> Array.to_list
-      |> List.filter_map (fun pid_s ->
-             let pkg_path = path_of_pid_s pid_s in
-             if Sys.is_directory pkg_path then
-               let pkg_content = Pkg_file.load_pkg_content pkg_path in
-               Some (P.str_to_pid pid_s, P.str_to_pkg pkg_content)
-             else None)
-    in
-    pid_and_pkgs |> List.to_seq |> Table.of_seq
+  let set_local_store table' = local_table := table'
 
   (* local api *)
   let install pid pkg =
     Table.add !local_table pid pkg;
-    let pkg_path = path_of_pid pid in
-    Std.remove_dir pkg_path;
-
-    Pkg_file.save_pkg_content pkg_path pkg
+    Pkg_store.remove_pkg pid;
+    let pkg_path = Pkg_store.path_of_pid pid in
+    Pkg_store.save_pkg_content pkg_path pkg
 
   let uninstall pid =
     Table.remove !local_table pid;
-    let pkg_path = path_of_pid pid in
-    Std.remove_dir pkg_path
+    Pkg_store.remove_pkg pid
 
   let reset () = Std.remove_dir C.local_root
   let lookup pid = Table.find !local_table pid
@@ -98,26 +66,26 @@ module Make
 
   (* remote api *)
   let publish pid pkg =
-    let pkg_path = remote_path_of_pid pid in
+    let pkg_path = Pkg_store.remote_path_of_pid (P.pid_to_str pid) in
     Std.remove_dir pkg_path;
-    Pkg_file.save_pkg_content pkg_path pkg
+    Pkg_store.save_pkg_content pkg_path pkg
 
   let unpublish pid =
-    let pkg_path = remote_path_of_pid pid in
+    let pkg_path = Pkg_store.remote_path_of_pid (P.pid_to_str pid) in
     Std.remove_dir pkg_path
 
   let fetch pid =
-    let remote_pkg_path = remote_path_of_pid pid in
-    let pkg_content = Pkg_file.load_pkg_content remote_pkg_path in
-    let pkg_path = path_of_pid pid in
-    Pkg_file.save_pkg_content pkg_path (P.str_to_pkg pkg_content)
+    let remote_pkg_path = Pkg_store.remote_path_of_pid (P.pid_to_str pid) in
+    let pkg_content = Pkg_store.load_pkg_content remote_pkg_path in
+    let pkg_path = Pkg_store.path_of_pid pid in
+    Pkg_store.save_pkg_content pkg_path (P.str_to_pkg pkg_content)
 
   let remote_info () = info_of_table !remote_table
 
   let init () =
     if Sys.file_exists C.local_root then
-      let table = load_store C.local_root in
-      set_store table
+      let table = Pkg_store.load_store C.local_root |> Table.of_seq in
+      set_local_store table
     else Sys.mkdir C.local_root 0o755;
 
     if not (Sys.file_exists C.remote_root) then Sys.mkdir C.remote_root 0o755
--- packaging/store.ml
+++ packaging/store.ml
@@ -1,3 +1,47 @@
+open Std.File_infix
+
+module File_store_make
+    (P : Package.PACKAGE)
+    (C : Manager.CONFIG)
+    (PC : Manager.PKG_FILE_CONFIG) =
+struct
+  (* The table is the cached view of the store.
+      We may not need this. *)
+  module Table = Hashtbl.Make (struct
+    type t = P.pid
+
+    let equal = Std.fn_lift2 String.equal P.pid_to_str
+    let hash = Hashtbl.hash
+  end)
+
+  (* let delete_pkg pkg_path =  *)
+
+  let path_of_pid pid = C.local_root $/ P.pid_to_str pid
+  let remote_path_of_pid pid_s = C.remote_root $/ pid_s
+
+  let save_pkg_content pkg_path pkg =
+    if not (Sys.file_exists pkg_path) then Sys.mkdir pkg_path 0o755;
+    Std.write_file_all (pkg_path $/ PC.file_name) (P.pkg_to_str pkg)
+
+  let load_pkg_content pkg_path = Std.read_file_all (pkg_path $/ PC.file_name)
+
+  let remove_pkg pid =
+    let pkg_path = path_of_pid pid in
+    Std.remove_dir pkg_path
+
+  let load_store pkgm_root =
+    let pid_and_pkgs =
+      Sys.readdir pkgm_root |> Array.to_list
+      |> List.filter_map (fun pid_s ->
+             let pkg_path = path_of_pid (P.str_to_pid pid_s) in
+             if Sys.is_directory pkg_path then
+               let pkg_content = load_pkg_content pkg_path in
+               Some (P.str_to_pid pid_s, P.str_to_pkg pkg_content)
+             else None)
+    in
+    pid_and_pkgs |> List.to_seq
+end
+
 (*open Std.File_infix
 
    module Pkg_table = Hashtbl.Make (struct
